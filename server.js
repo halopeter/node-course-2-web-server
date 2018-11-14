e38f807015744d5810ddf2d5224059638be71e80
halopeter <flamesea02@gmail.com>
2018-11-14 18:11:41 +0100

    Add project page

--- server.js
+++ server.js
@@ -48,6 +48,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render(('projects.hbs'), {
+    pagetitle: 'Projects'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Error Handler'
